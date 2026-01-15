98f7b14508ec4635c5e7c5639ab29b845e081a52
kgosiamani <kgosifireheart@gmail.com>
2026-01-15 21:15:46 +0200

    Add files via upload

---
+++ js/tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#f3bac5",
+                "background-light": "#f8f6f6",
+                "background-dark": "#201215",
+                "accent-dark": "#1a0f11",
+            },
+            fontFamily: {
+                "display": ["Plus Jakarta Sans", "sans-serif"],
+                "serif": ["Playfair Display", "serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "0.75rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
